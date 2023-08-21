95c90b4b2a1a606ef0301a1ef1d108c6c7174f49
wilfrid keirsbilck <willkeirs@gmail.com>
2023-08-21 17:01:45 +0200

    costumeshow , rent form , costume form

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,2 +1,22 @@
 class CostumesController < ApplicationController
+  def show
+    @costume = Costume.find(params[:id])
+  end
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    @costume.user_id = current_user.id
+    @costume.save!
+    redirect_to costume_path(@costume)
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :description, :price, :size, :city)
+  end
 end
--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,21 @@
 class RentsController < ApplicationController
+  def new
+    @costume = Costume.find(params[:costume_id])
+    @rent = Rent.new
+  end
+
+  def create
+    @costume = Costume.find(params[:costume_id])
+    @rent = Rent.new(rent_params)
+    @rent.user = current_user
+    @rent.costume = @costume
+    @rent.save!
+    redirect_to costume_path(@costume)
+  end
+
+  private
+
+  def rent_params
+    params.require(:rent).permit(:begin_date, :end_date)
+  end
 end
