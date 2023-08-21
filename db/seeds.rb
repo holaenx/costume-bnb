5bbf0bcd0cb58f863de9e00da34ca260342194bd
wilfrid keirsbilck <willkeirs@gmail.com>
2023-08-21 17:08:08 +0200

    cece

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-require 'faker'
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-
-puts " generating costumes brbrbrbrbr"
-50.times do
-  costume = Costume.create!(
-    name: Faker::Name.name_with_middle,
-    description: Faker::Quote.yoda,
-    size: "M",
-    price: 12,
-    city: Faker::Address.city,
-    user_id: User.last.id
-  )
-  costume.save!
-end
-puts " costumes generated"
