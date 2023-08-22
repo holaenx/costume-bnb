34eadf3b406232afb7be43821009a4d34f3fe53e
Lorenzo Lara <hola.enx@gmail.com>
2023-08-22 17:47:13 +0200

    Show lien user profile et retour a l index des cotumes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  def custom
+    @user = User.find(params[:id])
+    @costumes = @user.costumes
+  end
 end
