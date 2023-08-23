9431275424126c184e9dc6a7f6df5fcd6caf4247
wilfrid keirsbilck <willkeirs@gmail.com>
2023-08-23 13:23:10 +0200

    sqdf

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require 'faker'
+
+Costume.destroy_all
+
+puts " generating costumes..."
+
+50.times do
+  costume = Costume.new(
+    name: Faker::Name.name,
+    description: Faker::Alphanumeric.alpha(number: 100),
+    size: ["XS","S","M","L","XL","XXL"].sample,
+    price: Faker::Config.random = Random.new(42),
+    city: ["Marseille","Paris","Nice"].sample,
+    user_id: user.all.sample,
+    photos: Cloudinary::Uploader.upload("san_francisco.jpg")
+  )
+ costume.save!
+end
+puts 'Finished!'
